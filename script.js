eff7b0ba3110062b8254881ff81fdf2f3ec573d0
ritika-sharma2003 <ritusharma3221@gmail.com>
2025-03-18 21:28:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let value = e.target.innerHTML;
+        
+        if (value === '=') {
+            try {
+                string = eval(string);
+            } catch {
+                string = "Error";
+            }
+            input.value = string;
+        } else if (value === 'AC') {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else {
+            string += value;
+            input.value = string;
+        }
+    });
+});
